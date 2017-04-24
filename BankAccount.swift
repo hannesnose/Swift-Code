a033d69581af8f69955dbf3bdfef15df12fb869d
Hannes Nose <hannesnose@users.noreply.github.com>
2017-04-24 09:00:46 -0400

    Update BankAccount.swift

--- BankAccount.swift
+++ BankAccount.swift
@@ -1,5 +1,7 @@
 
 
+//: Playground - noun: a place where people can play
+
 import UIKit
 
 class BankAccount {
@@ -9,7 +11,6 @@ class BankAccount {
     var transactionDescription: String
     
     
-    
     init(currentBalanceChecking: Float, currentBalanceSavings: Float,transaction: Float, transactionDescription:String) {
         self.currentBalanceChecking = currentBalanceChecking
         self.transaction = transaction
@@ -17,66 +18,94 @@ class BankAccount {
         self.currentBalanceSavings = currentBalanceSavings
     }
     
-    //Making purchase with funds from checking account
-    
-    func withdrawalTransaction (transaction: Float, transactionDescription: String){
-        
-        if transaction <= currentBalanceChecking {
-            let deductedAmount = currentBalanceChecking - transaction
-            currentBalanceChecking = deductedAmount
-            print("Item: \(transactionDescription). Amount: $\(transaction). You have $\(currentBalanceChecking) left in your checking account")
-        } else {
-            print("You have insufficient Funds")
-            currentBalanceChecking
-        }
-        
-    }
     // Prevent negative deposits
     
     func checkIfNumberIsNegative(input:Float) -> Float {
-        if input < 0 {
-            print("You can not have negative input. Please check deposit amount")
+        if input <= 0  {
+            print("\(input) is not a authorized amount.  Please check deposit")
         }
         return input
+    }
+    
+    //Making withdrawals with funds from checking account
+    
+    func withdrawalTransactionFromChecking (transaction: Float, transactionDescription: String){
+        
+        if transaction > currentBalanceChecking || transaction <= 0 {
+            print("You have insufficient funds for your transaction.",currentBalanceChecking, "is your current balance. Step your game up!")
+        } else {
+            let deductedAmount = currentBalanceChecking - transaction
+            currentBalanceChecking = deductedAmount
+            print("Item: \(transactionDescription). Amount: $\(transaction). You have $\(currentBalanceChecking) left in your checking account")
+        }
     }
     
     // Depositing money into your checking account
     
     func depositFundsInChecking(currentCheckingBalance: Float, transaction: Float)->Float {
-        
-        let deposit = currentBalanceChecking + checkIfNumberIsNegative(input: transaction)
-        currentBalanceChecking = deposit
+        if transaction <= 0 {
+            print("You are attempting a transaction that can not be completed")
+        } else {
+            let deposit = currentBalanceChecking + checkIfNumberIsNegative(input: transaction)
+            currentBalanceChecking = deposit
+        }
         return currentBalanceChecking
     }
     
     // Depositing money into your savings account
     
-    func depositFundsInSavings(currentSavingBalance: Float, transaction: Float)->Float {
+    func depositFundsInSavings(currentSavingsBalance: Float, transaction: Float)->Float {
+        if transaction <= 0 {
+            print("You are attempting a transaction that can not be completed")
+        } else {
+            let deposit = currentSavingsBalance + checkIfNumberIsNegative(input: transaction)
+            let currentBalanceSavings = deposit
+            //let currentSavingsBalance = currentBalanceSavings
+        }
+        return currentBalanceSavings
+    }
+    
+    
+    // Transferring money from Savings account to Checking account
+    
+    func transferFundsFromSavingstoCheckingAccount(currentSavingsBalance: Float, transaction: Float) -> Float{
+        if transaction <= 0 {
+            print("\(transaction) is not a authorized amount.  Please check deposit")
+        } else if currentSavingsBalance <= 0 || transaction > currentSavingsBalance {
+            print("\(currentSavingsBalance) is not a sufficient for the transaction action you are trying to perform")
+        } else {
+            let transferFromSavingstoChecking = currentBalanceSavings - transaction
+            let transferred = transaction + currentBalanceChecking
+            currentBalanceChecking =  transferred
+            currentBalanceSavings  = transferFromSavingstoChecking
+            print("You transferred $\(transaction) to your checking. Current balance of account is $\(currentBalanceSavings)")
+        }
         
-        let deposit = currentBalanceSavings + checkIfNumberIsNegative(input: transaction)
-        currentBalanceSavings = deposit
-        return currentBalanceSavings
-    }
-    
-    // Transferring money from checking account to savings account
-    
-    func transferFundstoSavings(currentCheckingBalance: Float, transaction: Float) -> Float{
-        let transferFromCheckingtoSavings = currentBalanceChecking - transaction
-        let transfer = -transferFromCheckingtoSavings + currentBalanceSavings
-        currentBalanceSavings = currentBalanceSavings + transfer
-        currentBalanceChecking  = transferFromCheckingtoSavings
-        return currentBalanceSavings
-    }
-    // Transferring money from savings account to checking account
-    
-    func transferFundstoChecking(currentSavingsBalance: Float, transaction: Float) -> Float{
-        let transferFromSavingstoChecking = currentBalanceSavings - transaction
-        let transfer = -transferFromSavingstoChecking + currentBalanceChecking
-        currentBalanceChecking = currentBalanceChecking + transfer
-        currentBalanceSavings  = transferFromSavingstoChecking
         return currentBalanceChecking
+        
     }
     
+    // Transferring money from Checking account to Savings account
+    
+    func transferFundsFromChecktoSavingsAccount(currentCheckingBalance: Float, transaction: Float) -> Float{
+        if transaction <= 0 {
+            print("\(transaction) is not a authorized amount.  Please check deposit")
+        } else if currentCheckingBalance <= 0 || transaction > currentCheckingBalance {
+            print("\(currentCheckingBalance) is not a sufficient for the transaction action you are trying to perform")
+        } else {
+            let transferFromCheckingstoSavings = currentCheckingBalance - transaction
+            
+            let transferred = transaction + currentBalanceSavings
+            currentBalanceSavings =  transferred
+            currentBalanceChecking  = transferFromCheckingstoSavings
+            print("You transferred $\(transaction) to your savings. Current balance of account is $\(currentBalanceChecking)")
+        }
+        
+        return currentBalanceSavings
+        
+    }
+}
+
 }
 
 
